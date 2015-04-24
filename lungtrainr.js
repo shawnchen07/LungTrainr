6097f3d95fcb7bf2285926ec2dcb2cc357e5fd73
Shawn Chen <syc010@ucsd.edu>
2015-04-24 10:24:50 -0700

    Added stopwatch
    Includes start, pause, and reset functionality.

--- lungtrainr.js
+++ lungtrainr.js
@@ -1,5 +1,5 @@
 function processBase() {
-	var debug = true;
+	var debug = false;
 
 	// Grab "form" values
 	var base = Math.floor(document.getElementById("base").value);
@@ -42,4 +42,50 @@ function processBase() {
 	if (debug)
 		console.log("outputstr", outputstr);
 	document.getElementById("output").innerHTML = outputstr;
+}
+
+window.onload = function() {
+	window.h1 = document.getElementsByTagName('h1')[0],
+		window.start = document.getElementById('start'),
+		window.stop = document.getElementById('stop'),
+		window.clear = document.getElementById('clear'),
+		window.seconds = 0, window.minutes = 0, window.hours = 0,
+		window.t, window.state = 0;
+}
+
+function startTimer() {
+	if (window.state != 1)
+		timer();
+}
+
+function timer() {
+	window.state = 1;
+	window.t = setTimeout(add, 1000);
+}
+
+function add() {
+	window.seconds++;
+	if (window.seconds >= 60) {
+		window.seconds = 0;
+		window.minutes++;
+		if (window.minutes >= 60) {
+			window.minutes = 0;
+			window.hours++;
+		}
+	}
+	
+	window.h1.textContent = (window.hours ? (window.hours > 9 ? window.hours : "0" + window.hours) : "00") + ":" + (window.minutes ? (window.minutes > 9 ? window.minutes : "0" + window.minutes) : "00") + ":" + (window.seconds > 9 ? window.seconds : "0" + window.seconds);
+
+	timer();
+}
+
+function stopTimer() {
+	window.state = 2;
+	clearTimeout(window.t);
+}
+
+function resetTimer() {
+	window.state = 3;
+	window.h1.textContent = "00:00:00";
+	window.seconds = 0; window.minutes = 0; window.hours = 0;
 }
